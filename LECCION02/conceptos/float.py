bb8ed6c3ac5f82754b44e97730b22536cc287c70
Cesar Hernandez <cehergra1999@gmail.com>
2025-04-24 19:28:42 -0500

    nuevas actividades

--- LECCION02/conceptos/float.py
+++
@@ -1,8 +0,0 @@
-
-numeroNegativo=-2.8
-numero=6.0
-numeroPositivo=6.7
-
-print(type(numeroNegativo).__name__)
-print(type(numero).__name__)
-print(type(numeroPositivo).__name__)
--- LECCION02/conceptos/int.py
+++
@@ -1,8 +0,0 @@
-
-numeroNegativo=-2
-numeroNeutro=0
-numeroPositivo=12
-
-print(type(numeroNegativo).__name__)
-print(type(numeroNeutro).__name__)
-print(type(numeroPositivo).__name__)
